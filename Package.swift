aa0f704804d91e140c02425fc2d5315875f32ead
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-03-05 23:27:36 -0800

    Initial Setup

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version:5.7
 
 //
-// This source file is part of the TemplatePackage open source project
-// 
+// This source file is part of the CardinalKit open-source project
+//
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
-// 
+//
 // SPDX-License-Identifier: MIT
 //
 
@@ -12,21 +12,42 @@ import PackageDescription
 
 
 let package = Package(
-    name: "TemplatePackage",
+    name: "CardinalKitFHIR",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v16)
     ],
     products: [
-        .library(name: "TemplatePackage", targets: ["TemplatePackage"])
+        .library(name: "CardinalKitFHIR", targets: ["CardinalKitFHIR"]),
+        .library(name: "CardinalKitFHIRMockDataStorageProvider", targets: ["CardinalKitFHIRMockDataStorageProvider"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/FHIRModels", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/StanfordBDHG/CardinalKit", .upToNextMinor(from: "0.3.3"))
     ],
     targets: [
         .target(
-            name: "TemplatePackage"
+            name: "CardinalKitFHIR",
+            dependencies: [
+                .product(name: "CardinalKit", package: "CardinalKit"),
+                .product(name: "ModelsR4", package: "FHIRModels")
+            ]
         ),
         .testTarget(
-            name: "TemplatePackageTests",
+            name: "CardinalKitFHIRTests",
             dependencies: [
-                .target(name: "TemplatePackage")
+                .target(name: "CardinalKitFHIR")
+            ]
+        ),
+        .target(
+            name: "CardinalKitFHIRMockDataStorageProvider",
+            dependencies: [
+                .target(name: "CardinalKitFHIR"),
+                .product(name: "CardinalKit", package: "CardinalKit"),
+                .product(name: "Views", package: "CardinalKit")
+            ],
+            resources: [
+                .process("Resources")
             ]
         )
     ]
---
+++ Tests/UITests/TestApp/FHIRMockDataStorageProviderTests/FHIRMockDataStorageProviderTestsView.swift
@@ -0,0 +1,59 @@
+//
+// This source file is part of the CardinalKit open-source project
+//
+// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+import CardinalKit
+import CardinalKitFHIR
+import CardinalKitFHIRMockDataStorageProvider
+import SwiftUI
+
+
+struct FHIRMockDataStorageProviderTestsView: View {
+    @EnvironmentObject var fhirStandard: FHIR
+    
+    
+    var body: some View {
+        MockUploadList()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Inject Observation") {
+                        injectNewObservations()
+                    }
+                }
+            }
+    }
+    
+    
+    private func injectNewObservations() {
+        let observations = [
+            Observation(
+                code: .init(),
+                id: UUID().uuidString.asFHIRStringPrimitive(),
+                status: FHIRPrimitive(.final)
+            )
+        ]
+        
+        _Concurrency.Task {
+            await fhirStandard.registerDataSource(
+                AsyncStream { continuation in
+                    for observation in observations {
+                        continuation.yield(.addition(observation))
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+#if DEBUG
+struct FHIRMockDataStorageProviderTestsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FHIRMockDataStorageProviderTestsView()
+    }
+}
+#endif
