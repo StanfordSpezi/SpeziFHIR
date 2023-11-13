05da4615f16d83c1ace3b22c2f66bf07474e698e
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-11-12 20:19:17 -0800

    Order

--- Sources/SpeziFHIR/FHIRStore.swift
+++ Sources/SpeziFHIR/FHIRStore.swift
@@ -12,54 +12,64 @@ import class ModelsR4.Bundle
 import enum ModelsDSTU2.ResourceProxy
 
 
+/// <#Description#>
 @Observable
 public class FHIRStore {
     @ObservationIgnored private var _resources: [FHIRResource]
     
     
-    var observations: [FHIRResource] {
-        access(keyPath: \.observations)
-        return _resources.filter { $0.category == .observation }
+    /// <#Description#>
+    public var allergyIntolerances: [FHIRResource] {
+        access(keyPath: \.allergyIntolerances)
+        return _resources.filter { $0.category == .allergyIntolerance }
     }
     
+    /// <#Description#>
     var conditions: [FHIRResource] {
         access(keyPath: \.conditions)
         return _resources.filter { $0.category == .condition }
     }
     
-    var encounters: [FHIRResource] {
-        access(keyPath: \.encounters)
-        return _resources.filter { $0.category == .encounter }
-    }
-    
+    /// <#Description#>
     var diagnostics: [FHIRResource] {
         access(keyPath: \.diagnostics)
         return _resources.filter { $0.category == .diagnostic }
     }
     
-    var procedures: [FHIRResource] {
-        access(keyPath: \.procedures)
-        return _resources.filter { $0.category == .procedure }
+    /// <#Description#>
+    var encounters: [FHIRResource] {
+        access(keyPath: \.encounters)
+        return _resources.filter { $0.category == .encounter }
     }
     
+    /// <#Description#>
     var immunizations: [FHIRResource] {
         access(keyPath: \.immunizations)
         return _resources.filter { $0.category == .immunization }
     }
     
-    var allergyIntolerances: [FHIRResource] {
-        access(keyPath: \.allergyIntolerances)
-        return _resources.filter { $0.category == .allergyIntolerance }
-    }
-    
+    /// <#Description#>
     var medications: [FHIRResource] {
         access(keyPath: \.medications)
         return _resources.filter { $0.category == .medication }
     }
     
+    /// <#Description#>
+    var observations: [FHIRResource] {
+        access(keyPath: \.observations)
+        return _resources.filter { $0.category == .observation }
+    }
+    
+    /// <#Description#>
     var otherResources: [FHIRResource] {
         access(keyPath: \.otherResources)
         return _resources.filter { $0.category == .other }
+    }
+    
+    /// <#Description#>
+    var procedures: [FHIRResource] {
+        access(keyPath: \.procedures)
+        return _resources.filter { $0.category == .procedure }
     }
     
     
@@ -70,12 +80,8 @@ public class FHIRStore {
     
     public func insert(resource: FHIRResource) {
         switch resource.category {
-        case .observation:
-            withMutation(keyPath: \.observations) {
-                _resources.append(resource)
-            }
-        case .encounter:
-            withMutation(keyPath: \.encounters) {
+        case .allergyIntolerance:
+            withMutation(keyPath: \.allergyIntolerances) {
                 _resources.append(resource)
             }
         case .condition:
@@ -86,29 +92,35 @@ public class FHIRStore {
             withMutation(keyPath: \.diagnostics) {
                 _resources.append(resource)
             }
-        case .procedure:
-            withMutation(keyPath: \.procedures) {
+        case .encounter:
+            withMutation(keyPath: \.encounters) {
                 _resources.append(resource)
             }
         case .immunization:
             withMutation(keyPath: \.immunizations) {
                 _resources.append(resource)
             }
-        case .allergyIntolerance:
-            withMutation(keyPath: \.allergyIntolerances) {
-                _resources.append(resource)
-            }
         case .medication:
             withMutation(keyPath: \.medications) {
+                _resources.append(resource)
+            }
+        case .observation:
+            withMutation(keyPath: \.observations) {
                 _resources.append(resource)
             }
         case .other:
             withMutation(keyPath: \.otherResources) {
                 _resources.append(resource)
             }
+        case .procedure:
+            withMutation(keyPath: \.procedures) {
+                _resources.append(resource)
+            }
         }
     }
     
+    /// <#Description#>
+    /// - Parameter bundle: <#bundle description#>
     public func load(bundle: Bundle) {
         let resourceProxies = bundle.entry?.compactMap({ $0.resource }) ?? []
         
@@ -117,26 +129,27 @@ public class FHIRStore {
         }
     }
     
+    /// <#Description#>
     public func removeAllResources() {
         // Not really ideal but seems to be a path to ensure that all observables are called.
-        _$observationRegistrar.willSet(self, keyPath: \.observations)
-        _$observationRegistrar.willSet(self, keyPath: \.encounters)
+        _$observationRegistrar.willSet(self, keyPath: \.allergyIntolerances)
         _$observationRegistrar.willSet(self, keyPath: \.conditions)
         _$observationRegistrar.willSet(self, keyPath: \.diagnostics)
-        _$observationRegistrar.willSet(self, keyPath: \.procedures)
+        _$observationRegistrar.willSet(self, keyPath: \.encounters)
         _$observationRegistrar.willSet(self, keyPath: \.immunizations)
-        _$observationRegistrar.willSet(self, keyPath: \.allergyIntolerances)
         _$observationRegistrar.willSet(self, keyPath: \.medications)
+        _$observationRegistrar.willSet(self, keyPath: \.observations)
         _$observationRegistrar.willSet(self, keyPath: \.otherResources)
+        _$observationRegistrar.willSet(self, keyPath: \.procedures)
         _resources = []
-        _$observationRegistrar.didSet(self, keyPath: \.observations)
-        _$observationRegistrar.didSet(self, keyPath: \.encounters)
+        _$observationRegistrar.didSet(self, keyPath: \.allergyIntolerances)
         _$observationRegistrar.didSet(self, keyPath: \.conditions)
         _$observationRegistrar.didSet(self, keyPath: \.diagnostics)
-        _$observationRegistrar.didSet(self, keyPath: \.procedures)
+        _$observationRegistrar.didSet(self, keyPath: \.encounters)
         _$observationRegistrar.didSet(self, keyPath: \.immunizations)
-        _$observationRegistrar.didSet(self, keyPath: \.allergyIntolerances)
         _$observationRegistrar.didSet(self, keyPath: \.medications)
+        _$observationRegistrar.didSet(self, keyPath: \.observations)
         _$observationRegistrar.didSet(self, keyPath: \.otherResources)
+        _$observationRegistrar.didSet(self, keyPath: \.procedures)
     }
 }
