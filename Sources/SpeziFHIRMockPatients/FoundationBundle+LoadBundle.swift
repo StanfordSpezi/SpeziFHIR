6b9bd434286451848aad85ada87f1e7c10b0345a
Lukas Kollmer <hey@lukaskollmer.de>
2025-05-28 14:35:19 -0700

    this doesn't need to be async

--- Sources/SpeziFHIRMockPatients/FoundationBundle+LoadBundle.swift
+++ Sources/SpeziFHIRMockPatients/FoundationBundle+LoadBundle.swift
@@ -14,16 +14,13 @@ extension Foundation.Bundle {
     /// Loads a FHIR `Bundle` from a Foundation `Bundle`.
     /// - Parameter name: Name of the JSON file in the Foundation `Bundle`
     /// - Returns: The FHIR `Bundle`
-    public func loadFHIRBundle(withName name: String) async -> Bundle {
+    public func loadFHIRBundle(withName name: String) -> Bundle {
         guard let resourceURL = Self.module.url(forResource: name, withExtension: "json") else {
             fatalError("Could not find the resource \"\(name)\".json in the SpeziFHIRMockPatients Resources folder.")
         }
-
         do {
-            return try JSONDecoder().decode(
-                Bundle.self,
-                from: Data(contentsOf: resourceURL)
-            )
+            let data = try Data(contentsOf: resourceURL)
+            return try JSONDecoder().decode(Bundle.self, from: data)
         } catch {
             fatalError("Could not decode the FHIR bundle named \"\(name).json\": \(error)")
         }
